32a2d26810e0f253aa574dc31a53fcf72eb996ee
Bhimesh Chauhan <bhimeshchauhan@gmail.com>
2025-12-28 21:04:17 +0530

    feat: config update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,23 @@
 /** @type {import('next').NextConfig} */
+const repoName = 'yenken';
+
 const nextConfig = {
   reactStrictMode: true,
+
   compiler: {
     styledComponents: true
   },
+
+  // REQUIRED for GitHub Pages (project repo)
   output: 'export',
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+
   images: {
     unoptimized: true
-  }
+  },
+
+  trailingSlash: true
 };
 
 module.exports = nextConfig;
